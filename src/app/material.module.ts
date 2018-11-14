cace895f0a26ae7f188822938d741a691e7d4e02
cronie3 <e.cronin@emc.com>
2018-11-14 10:49:38 +0000

    first routes and initial material

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,10 +1,13 @@
-import {NgModule} from '@angular/core';
-import {MatButtonModule, MatIconModule} from '@angular/material';
+import { NgModule } from '@angular/core';
+import {
+  MatButtonModule,
+  MatIconModule,
+  MatFormFieldModule,
+  MatInputModule
+} from '@angular/material';
 
 @NgModule({
-  imports: [MatButtonModule, MatIconModule],
-  exports: [MatButtonModule, MatIconModule]
+  imports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule],
+  exports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule]
 })
-
-export class MaterialModule {
-}
+export class MaterialModule {}
